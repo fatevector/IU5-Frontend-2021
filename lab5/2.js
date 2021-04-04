71e71cd01d8899952ce596b507e743bdfdc213bd
Иван Петров <ivan@MacBook-Pro.local>
2021-04-04 19:28:36 +0300

    Убрал лишнее

--- lab5/2.js
+++ lab5/2.js
@@ -12,16 +12,6 @@
  * console.log(curry(add)(1)(2, 3)); //6
  * console.log(curry(add)(1, 2, 3)); //6
  */
-// function curry(func) {
-//     //for (let arg in args) console.log(arg);//code here
-//     // return function(...args){
-//     //     func.
-//     // }
-//     return function(...args){
-//         if (func.length<=args.length) return func(...args)
-//         else return func.bind(null, ...args)
-//     }
-// }
 function curry(func) {
     return (...args)=>{
         if (func.length<=args.length) return func(...args);
