ad8b1e01aebd91e646db4e2f01286a3fb229d60c
Иван Петров <ivan@MacBook-Pro.local>
2021-05-02 02:10:50 +0300

    Выполнил девятую работу

--- lab9/src/App.js
+++ lab9/src/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import "./App.css";
 import Home from "./components/Home";
 
 import User from "./components/User";
@@ -30,55 +29,7 @@ function App() {
       public_repos: info.public_repos
     });
   }
-  // function MakeRequest(text) {
-  //   console.log("Creating a request: " + text);
-  //   fetch('https://api.github.com/users/' + text)
-  //   .then((result) => result.json())
-  //   .then((data) => {
-  //     if (data.message) {
-  //       setError(true);
-  //     } else {
-  //       setError(false);
-  //       setData(data);
-  //     }
-  //     console.log(data);
-  //   })
-  //   .catch(() => {
-  //     setError(true);
-  //   });
-  // }
-  // let git = "fatevector";
-  // useEffect(() => {
-  //   fetch(`https://api.github.com/users/${git}`)
-  //     .then((result) => result.json())
-  //     .then((res) => {
-  //       console.log(res);
-  //     });
-  // });
-  // const [value, setValue]=useState('initial');
-  // const renderCount=useRef(1);
-  // useEffect(()=>{
-  //   renderCount.current++;
-  // })
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    /* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1>Количество рендеров: {renderCount.current}</h1>
-        <input type="text" onChange={e=>setValue(e.target.value)} value={value}/>     
-      </header>
-    </div> */
     <Router>
       <Switch>
         <Route path="/user">
